13eea8cdd22fc2e3bed78f303bbc7c3938fe6b0d
marcingal <marcin921125@gmail.com>
2018-11-22 10:18:13 +0100

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+class Counter extends React.Component {
+    state = {
+        number: this.props.startValue
+    }
+
+    incHandler = () => {
+        if (this.state.number >= this.props.max) return
+        this.setState({ number: this.state.number + 1 })
+    }
+
+    decHandler = () => {
+        if (this.state.number <= this.props.min) return
+
+        this.setState({ number: this.state.number - 1 })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.number}</h2>
+                <button
+                    onClick={this.incHandler}>+
+                </button>
+                <button
+                    onClick={this.decHandler}>-
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Counter
